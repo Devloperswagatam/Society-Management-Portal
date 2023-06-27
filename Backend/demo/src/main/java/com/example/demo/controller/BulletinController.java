9973e762710ebbf44486544e7eb4dc2405ab7e91
anisha <a@gmail.com>
2023-06-27 21:58:46 +0530

    added bulletin

---
+++ Backend/demo/src/main/java/com/example/demo/controller/BulletinController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Bulletin;
+import com.example.demo.repository.BulletinRepository;
+@RestController
+@RequestMapping("/notes")
+public class BulletinController {
+	@Autowired
+	BulletinRepository bulletinRepo;
+	
+	@PostMapping("/note")
+	public String createNotes(@RequestBody Bulletin bulletin) {
+		bulletinRepo.save(bulletin);
+		return "Notes has been created successfully";
+	}
+	@GetMapping("/note")
+	public ResponseEntity<List<Bulletin>> getAllNotes(){
+		List<Bulletin> notelist = new ArrayList<>();
+		bulletinRepo.findAll().forEach(notelist::add);
+		return new ResponseEntity<List<Bulletin>>(notelist, HttpStatus.OK);
+	}
+}
--- Backend/demo/src/main/java/com/example/demo/repository/BulletinRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repository/BulletinRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public interface BulletinRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Bulletin;
 
+@Repository
+public interface BulletinRepository extends JpaRepository<Bulletin, Integer>{
+	
 }
